00bb400879cec146d5fd6a1f84116f08190b5553
Glory Malamsha <96989410+Glory-Mal@users.noreply.github.com>
2023-02-13 18:59:14 +0300

    Add files via upload

---
+++ main.dart
@@ -0,0 +1,33 @@
+import 'package:flutter/material.dart';
+
+void main() {
+  return runApp(
+    MaterialApp(
+      home: Scaffold(
+        backgroundColor: Colors.red,
+        appBar: AppBar(
+          title: Text('Dicee'),
+          backgroundColor: Colors.red,
+        ),
+        body: DicePage(),
+      ),
+    ),
+  );
+}
+
+class DicePage extends StatelessWidget {
+  @override
+  Widget build(BuildContext context) {
+    return Row(children: <Widget>[
+      Expanded(
+        child: Image(
+          image: AssetImage('images/dice1.png'),
+        ),
+      ), Expanded(
+        child: Image(
+          image: AssetImage('images/dice1.png'),
+        ),
+      )
+    ]);
+  }
+}
