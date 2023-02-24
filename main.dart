775da86d89444574242d73ecba71263e9660eb5f
Glory Malamsha <96989410+Glory-Mal@users.noreply.github.com>
2023-02-24 15:25:21 +0300

    Both dice changes
    when either dice is clicked, both dice changes

--- main.dart
+++ main.dart
@@ -36,6 +36,7 @@ class _DicePageState extends State<DicePage> {
             onPressed: () {
               setState(() {
                 leftDiceNumber = Random().nextInt(6)+1;
+                rightDiceNumber=Random().nextInt(6)+1;
               });
 
             },
@@ -46,6 +47,7 @@ class _DicePageState extends State<DicePage> {
           child: TextButton(
             onPressed: () {
               setState(() {
+                leftDiceNumber = Random().nextInt(6)+1;
                 rightDiceNumber=Random().nextInt(6)+1;
               });
             },
