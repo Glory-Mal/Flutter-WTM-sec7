73c86936b8e52d3cc3a9c7fbf7a236e0262b43b3
Glory Malamsha <96989410+Glory-Mal@users.noreply.github.com>
2023-02-26 16:18:14 +0300

    Dice app has a function
    A function for repeated words is created.
    The function is called where needed

--- main.dart
+++ main.dart
@@ -27,6 +27,13 @@ class DicePage extends StatefulWidget {
 class _DicePageState extends State<DicePage> {
   int leftDiceNumber = 1;
   int rightDiceNumber = 1;
+  void changeDiceFace() {
+    setState(() {
+      leftDiceNumber = Random().nextInt(6) + 1;
+      rightDiceNumber = Random().nextInt(6) + 1;
+    });
+  }
+
   @override
   Widget build(BuildContext context) {
     return Center(
@@ -34,11 +41,7 @@ class _DicePageState extends State<DicePage> {
         Expanded(
           child: TextButton(
             onPressed: () {
-              setState(() {
-                leftDiceNumber = Random().nextInt(6)+1;
-                rightDiceNumber=Random().nextInt(6)+1;
-              });
-
+              changeDiceFace();
             },
             child: Image.asset('images/dice$leftDiceNumber.png'),
           ),
@@ -46,10 +49,7 @@ class _DicePageState extends State<DicePage> {
         Expanded(
           child: TextButton(
             onPressed: () {
-              setState(() {
-                leftDiceNumber = Random().nextInt(6)+1;
-                rightDiceNumber=Random().nextInt(6)+1;
-              });
+              changeDiceFace();
             },
             child: Image.asset('images/dice$rightDiceNumber.png'),
           ),
